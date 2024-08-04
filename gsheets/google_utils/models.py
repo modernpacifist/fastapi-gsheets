463e091427f23f01167af1b7ba4762bd1200126d
modernpacifist <modernpacifist@gmail.com>
2024-08-04 17:00:01 +0000

    docker-cron:04/08/2024-17:00:00 automated backup commit

--- gsheets/google_utils/models.py
+++ gsheets/google_utils/models.py
@@ -2,4 +2,19 @@ from marshmallow import Schema, fields
 
 
 class Record(Schema):
-    
+    id = fields.Int()
+    id_str = fields.Str()
+    google_drive_directory = fields.Str()
+    conference_title_full_ru = fields.Str()
+    conference_title_short_ru = fields.Str()
+    conference_title_full_en = fields.Str()
+    conference_title_short_en = fields.Str()
+    organization_name = fields.Str()
+    applications_opening_date = fields.Date(format="%d.%m.%Y")
+    applications_closing_date = fields.Date(format="%d.%m.%Y")
+    articles_opening_date = fields.Date(format="%d.%m.%Y")
+    articles_closing_date = fields.Date(format="%d.%m.%Y")
+    conference_start_date = fields.Date(format="%d.%m.%Y")
+    conference_end_date = fields.Date(format="%d.%m.%Y")
+    conference_url = fields.URL()
+    organizator_email = fields.Str()

ff39342909d4bbf6bf9f44f392fd8b474fafacfe
modernpacifist <modernpacifist@gmail.com>
2024-08-07 16:00:03 +0000

    docker-cron:07/08/2024-16:00:00 automated backup commit

--- gsheets/google_utils/sheets.py
+++ gsheets/google_utils/sheets.py
@@ -6,26 +6,37 @@ SACC = auth.setup_account()
 sheets_conf = google_sheets()
 
 SPREADSHEET_ID = sheets_conf.get('id')
+LIST = sheets_conf.get('list')
 RANGE = sheets_conf.get('range')
 
 
-def get_last_row():
-    return SACC.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range='A2:A').execute()
+def _get_last_empty_range():
+    r = SACC.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=f'{LIST}!A2:A').execute()
+    if not r:
+        return None
+    return r
 
 
 def get_all_conferences():
     # return SACC.spreadsheets().values().batchGet(spreadsheetId=SPREADSHEET_ID, ranges=RANGE).execute()
-    return get_last_row()
+    return _get_last_empty_range()
 
 
 def add_conference():
+    lr = _get_last_empty_range()
+    if not lr:
+        return None
+    
+    print(lr)
+    return None
+
     body = {
         'values': [
-            ['sample', 'title'],
-            ['sample1', 'heee']
+            ['sample', 'title']
         ]
     }
-    r = SACC.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID, range=RANGE, valueInputOption="RAW", body=body).execute()
+
+    r = SACC.spreadsheets().values().append(spreadsheetId=SPREADSHEET_ID, range=f'{LIST}!{RANGE}', valueInputOption="RAW", body=body).execute()
     return r
 
 

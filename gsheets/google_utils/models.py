a979816038aad030851fc853df3e7e93b7de6c3d
modernpacifist <modernpacifist@gmail.com>
2024-08-12 14:15:02 +0000

    DESKTOP-UR392UR:12/08/2024-14:15:00 automated backup commit

--- gsheets/google_utils/models.py
+++ gsheets/google_utils/models.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from marshmallow import Schema, fields
 from pydantic import BaseModel, Field, HttpUrl, EmailStr, field_validator, field_serializer
 from datetime import datetime, date
@@ -23,12 +24,6 @@ from typing import Optional
 #     organizator_email = fields.Str(required=True)
 
 
-class Record(BaseModel):
-    @abstractmethod
-    def convert_to_list(self):
-        raise NotImplementedError()
-
-
 class GetConference(Schema):
     id = fields.Int(required=True)
     name_rus = fields.Str(required=True)
@@ -46,21 +41,31 @@ class GetConference(Schema):
     email = fields.Email(required=True)
 
 
-class PostConference(BaseModel):
+class ConferenceInfo(BaseModel):
+    id: int
     google_spreadsheet: str
+    google_drive_directory_id: str
     name_rus: str
     name_rus_short: str
     name_eng: Optional[str] = ""
     name_eng_short: Optional[str] = ""
+    organized_by: str
     registration_start_date: str
     registration_end_date: str
     submission_start_date: str
     submission_end_date: str
     conf_start_date: str
     conf_end_date: str
-    organized_by: str
     url: Optional[str] = ""
     email: EmailStr
+
+    @abstractmethod
+    def convert_to_list(self):
+        raise NotImplementedError()
+
+
+class PostConference(ConferenceInfo):
+    id: int = Field(default=0, exclude=True)
 
     @field_validator(
             'registration_start_date',

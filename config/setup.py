54c2a84f1b989f021a515a66ba752339eb239189
modernpacifist <modernpacifist@gmail.com>
2024-08-24 10:04:29 +0300

    commit

---
+++ config/setup.py
@@ -0,0 +1,56 @@
+import os
+
+from configparser import ConfigParser
+from dataclasses import dataclass, field
+
+
+@dataclass
+class TelegramConfig:
+    token: str
+
+
+@dataclass
+class SheetsConfig:
+    uri: str
+    get_uri: str = field(init=False)
+    post_uri: str = field(init=False)
+    put_uri: str = field(init=False)
+
+    def __post_init__(self):
+        self.get_uri = f'http://{self.uri}/conferences'
+        self.get_single_uri = f'http://{self.uri}/conferences/'
+        self.post_uri = f'http://{self.uri}/conferences'
+        self.put_uri = f'http://{self.uri}/conferences/'
+
+
+def setup(section, filename='config.ini'):
+    return_object = None
+    if section == 'telegram bot':
+        return_object = TelegramConfig
+
+    if section == 'sheets':
+        return_object = SheetsConfig
+
+    if not return_object:
+        raise Exception('No such config available')
+
+    if not os.path.isfile(filename):
+        raise Exception('No config.ini file found')
+
+    if section is None:
+        raise Exception('You did not specify the section to parse')
+
+    parser = ConfigParser()
+    parser.read(filename)
+
+    if not parser.has_section(section):
+        raise Exception(f'File {filename} does not have section {section}')
+
+    params = parser.items(section)
+
+    try: 
+        return return_object(**{i: k for i, k in params})
+
+    except Exception as e:
+        print(e)
+        exit(1)

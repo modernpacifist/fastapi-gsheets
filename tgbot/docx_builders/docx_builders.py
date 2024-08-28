4527d671baa2d9e293fbf0ff6ab54bdbed3b4d32
modernpacifist <modernpacifist@gmail.com>
2024-08-28 11:30:03 +0000

    DESKTOP-UR392UR:28/08/2024-11:30:00 automated backup commit

--- tgbot/docx_builders/docx_builders.py
+++ tgbot/docx_builders/docx_builders.py
@@ -1,26 +1,23 @@
 import docx
 from abc import ABC, abstractmethod
-# import abc
 
 
 class DocxBuilder(ABC):
     def __init__(self, filename, data):
         self.filename = filename
         self.data = data
-        self.document = docx.Document()
 
     @abstractmethod
-    def generate(self):
-        raise NotImplemented(f'{__class__}.generate is not implemented')
+    def create(self):
+        raise NotImplementedError('Method not implemented')
 
     def save(self, path):
         self.document.save(f'{path}/{self.filename}')
 
 
 class ApplicationsReport(DocxBuilder):
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, filename, data):
+        super().__init__(filename, data)
 
 
 class ConferenceReport(DocxBuilder):

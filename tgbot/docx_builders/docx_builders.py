210a870195fecbec927561478286e747358bdb4e
modernpacifist <modernpacifist@gmail.com>
2024-08-28 11:45:03 +0000

    DESKTOP-UR392UR:28/08/2024-11:45:00 automated backup commit

--- tgbot/docx_builders/docx_builders.py
+++ tgbot/docx_builders/docx_builders.py
@@ -6,18 +6,33 @@ class DocxBuilder(ABC):
     def __init__(self, filename, data):
         self.filename = filename
         self.data = data
+        self.doc = None
 
     @abstractmethod
     def create(self):
         raise NotImplementedError('Method not implemented')
 
-    def save(self, path):
-        self.document.save(f'{path}/{self.filename}')
+    def save(self, path='./'):
+        self.doc.save(f'{path}/{self.filename}')
 
 
 class ApplicationsReport(DocxBuilder):
-    def __init__(self, filename, data):
-        super().__init__(filename, data)
+    def create(self, data):
+        def distribute_data(data):
+            return data[0]
+
+        data = distribute_data(data)
+
+        self.doc = docx.Document()
+        self.doc.add_heading(data, 0)
+        self.doc.save(data)
+
+    def save(self, path='.'):
+        try:
+            self.doc.save(f'{path}/{self.filename}')
+
+        except Exception as e:
+            print(e)
 
 
 class ConferenceReport(DocxBuilder):

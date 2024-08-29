84fa3bf335879f273125cdadcdf0448cd9692248
modernpacifist <modernpacifist@gmail.com>
2024-08-29 13:00:03 +0000

    DESKTOP-UR392UR:29/08/2024-13:00:00 automated backup commit

--- tgbot/gdrive/operations.py
+++ tgbot/gdrive/operations.py
@@ -12,24 +12,33 @@
 #         return None
 
 
-def get_folder_files(conf, parent_folder_id, folder=None):
+def get_folder_files(conf, parent_folder_id, folder):
     try:
         results = (
             conf.sacc.files()
-            # .list(q=f"mimeType = 'application/vnd.google-apps.folder' and '{parent_folder_id}' in parents and name = '{folder}'",
-            .list(q=f"'{parent_folder_id}' in parents and name = '{folder}'",
+            .list(q=f"'{parent_folder_id}' in parents and name='{folder}'",
+                  pageSize=10,
+                  spaces='drive',
+                  fields="files(id)")
+            .execute()
+        )
+
+        parent_folder_id = results.get()
+        results = (
+            conf.sacc.files()
+            .list(q=f"'{parent_folder_id}' in parents and name='{folder}'",
                   pageSize=10,
                   spaces='drive',
                   fields="nextPageToken, files(id, name)")
             .execute()
         )
 
-        if folder:
-            for f in results.get('files', []):
-                if folder == f.get('name'):
-                    return f.get('id')
-                # print()
-            return results
+        # if folder:
+        #     for f in results.get('files', []):
+        #         if folder == f.get('name'):
+        #             return f.get('id')
+        #         # print()
+        #     return results
 
         return results
 
